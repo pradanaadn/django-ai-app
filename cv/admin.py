d60955dc1e56a750e3ef13c7e4eadc1764084d8e
pradanaadn <pradanaadn21@gmail.com>
2024-10-20 16:23:27 +0800

    feat: add model cv in admin panel

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import Cv, DetailCv, DetailSection, Section
 # Register your models here.
+
+class CvAdmin(admin.ModelAdmin):
+    fieldsets = [
+        "user", "file"
+    ]
+class DetailCvAdmin(admin.ModelAdmin):
+    fieldsets = [
+        "cv", "detail_section", "konten"
+    ]   
+    
+class SectionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        "nama","deskripsi", "active"
+    ]
+
+class DetailSectionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        "section", "nama", "deskripsi"
+    ]
+
+
+
+
+
+admin.site.register(Cv, CvAdmin)
+admin.site.register(DetailCv, DetailCvAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(DetailSection, DetailSectionAdmin)
